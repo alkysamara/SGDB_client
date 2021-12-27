b2ae91f69b3144bba27aebeebe25c17ec85fe145
alkysamara <alky_samara@mail.ru>
2021-12-27 17:10:58 +0400

    added templates for add and select component type

---
+++ SGDBclient/FormAddComponentType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; //use in pm console: Install-Package MySql.Data
+
+namespace SGDBclient {
+	public partial class FormAddComponentType : Form {
+		private MySql.Data.MySqlClient.MySqlConnection SQLconnection;
+		public FormAddComponentType(MySql.Data.MySqlClient.MySqlConnection con) {
+			InitializeComponent();
+			SQLconnection = con;
+			dateTimePicker1.Format = DateTimePickerFormat.Custom;
+			dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e) {
+			if (textBoxTotalCost.Text == "") {
+				textBoxTotalCost.Text = "0";
+			}
+			try {
+				MySqlCommand command = new MySqlCommand("INSERT INTO sgitemsdb.orders (`Date`, `Comment`, `TotalCost`, `PaySource`) VALUES(\'" +
+					dateTimePicker1.Text + "\',\'" +
+					textBoxComment.Text + "\',\'" +
+					textBoxTotalCost.Text + "\',\'" +
+					textBoxPaySource.Text + "\')", SQLconnection);
+				command.ExecuteNonQuery();
+				this.Close();
+			} catch (Exception ee) {
+				MessageBox.Show(ee.Message);
+			}
+		}
+
+		private void btnClick_Click(object sender, EventArgs e) {
+			this.Close();
+		}
+	}
+}
---
+++ SGDBclient/FormSelectComponentType.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; //use in pm console: Install-Package MySql.Data
+
+namespace SGDBclient {
+	public partial class FormSelectComponentType : Form {
+		public int selectedComponentID = 0;
+		public string selectedComponentName = "";
+		private MySql.Data.MySqlClient.MySqlConnection SQLconnection;
+		private void updateTable() {
+			MySqlDataReader reader;
+			try {
+				MySqlCommand command = new MySqlCommand("SELECT * FROM Components " + 
+					"WHERE Components.PartNumber LIKE \'%" + textBoxSearchString.Text + "%\'", SQLconnection);
+				reader = command.ExecuteReader();
+			} catch (Exception e) {
+				MessageBox.Show(e.Message);
+				return;
+			}
+			dataGridView1.Rows.Clear();
+			dataGridView1.Columns.Clear();
+			for (int i = 0; i < reader.FieldCount; i++) {
+				dataGridView1.Columns.Add(reader.GetName(i), reader.GetName(i));
+				if (reader.GetName(i).StartsWith("id")) { //this is an id field, hide it
+					dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.None; //switch off autosize
+					dataGridView1.Columns[i].Width = 1; //minimal width to 'hide' it
+				}
+			}
+			while (reader.Read()) {
+				dataGridView1.Rows.Add();
+				for (int j = 0; j < reader.FieldCount; j++)
+					dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[j].Value = reader[j];
+			}
+			reader.Close();
+		}
+		public FormSelectComponentType(MySql.Data.MySqlClient.MySqlConnection con) {
+			InitializeComponent();
+			SQLconnection = con;
+			updateTable();
+		}
+
+		private void buttonSelect_Click(object sender, EventArgs e) {
+			try {
+				selectedComponentID = (int)dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].Cells[0].Value;
+				selectedComponentName = (string)dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].Cells[1].Value;
+			} catch (Exception) {
+				selectedComponentID = 0;
+			}
+			this.Close();
+		}
+
+		private void buttonRefresh_Click(object sender, EventArgs e) {
+			updateTable();
+		}
+
+		private void dataGridView1_KeyDown(object sender, KeyEventArgs e) {
+			e.SuppressKeyPress = true;
+			if (e.KeyCode == Keys.Enter) {
+				buttonSelect_Click(sender, e);
+			}
+		}
+
+		private void dataGridView1_DoubleClick(object sender, EventArgs e) {
+			buttonSelect_Click(sender, e);
+		}
+
+		private void btnAddComponent_Click(object sender, EventArgs e) {
+			FormAddComponentType form = new FormAddComponentType(SQLconnection);
+			form.Show();
+		}
+	}
+}

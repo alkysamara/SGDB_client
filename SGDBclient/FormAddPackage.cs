0bb33c1c7a200301a0df2adc0af8489c97527ce9
Sasha <sasha.sobornitskaya@gmail.com>
2022-01-08 10:37:10 +0400

    Packages

---
+++ SGDBclient/FormAddPackage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; //use in pm console: Install-Package MySql.Data
+
+namespace SGDBclient {
+	public partial class FormAddPackage : Form {
+
+		private MySql.Data.MySqlClient.MySqlConnection SQLconnection;
+		public FormAddPackage(MySql.Data.MySqlClient.MySqlConnection con) {
+			InitializeComponent();
+			SQLconnection = con;
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e) {
+			if (textBoxPackageName.Text == "") {
+				MessageBox.Show("Name cannot be empty!");
+				return;
+			}
+			try {
+				MySqlCommand command = new MySqlCommand("INSERT INTO sgitemsdb.storages (`PackageName`, `Type`, `Pins`, 'PackageParameters) VALUES(\'" +
+					textBoxPackageName.Text + "\',\'" +
+					textBoxPackageType.Text + "\',\'" +
+					numericUpDownPinCount.Text + "\',\'" +
+					textBoxComments.Text + "\')", SQLconnection);
+				command.ExecuteNonQuery();
+				this.Close();
+			} catch (Exception ee) {
+				MessageBox.Show(ee.Message);
+			}
+		}
+
+		private void btnClick_Click(object sender, EventArgs e) {
+			this.Close();
+		}
+
+        private void FormAddPackage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

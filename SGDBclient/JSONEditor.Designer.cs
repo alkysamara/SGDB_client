8007952acff38958d77865c2cfeb8c65b7484660
alky_desktop <alky_smara@mail.ru>
2023-08-16 22:53:28 +0400

    Fixed json query, better json editor window layout

--- SGDBclient/JSONEditor.Designer.cs
+++ SGDBclient/JSONEditor.Designer.cs
@@ -34,7 +34,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 77);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(407, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,6 +45,8 @@
             // 
             // tb_JSON
             // 
+            this.tb_JSON.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tb_JSON.Location = new System.Drawing.Point(12, 12);
             this.tb_JSON.Name = "tb_JSON";
             this.tb_JSON.Size = new System.Drawing.Size(470, 20);
@@ -51,9 +54,11 @@
             // 
             // tb_param_value
             // 
-            this.tb_param_value.Location = new System.Drawing.Point(99, 50);
+            this.tb_param_value.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_param_value.Location = new System.Drawing.Point(197, 51);
             this.tb_param_value.Name = "tb_param_value";
-            this.tb_param_value.Size = new System.Drawing.Size(75, 20);
+            this.tb_param_value.Size = new System.Drawing.Size(204, 20);
             this.tb_param_value.TabIndex = 2;
             // 
             // label1
@@ -68,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(96, 35);
+            this.label2.Location = new System.Drawing.Point(194, 35);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 4;
@@ -79,12 +84,13 @@
             this.cb_param_name.FormattingEnabled = true;
             this.cb_param_name.Location = new System.Drawing.Point(12, 50);
             this.cb_param_name.Name = "cb_param_name";
-            this.cb_param_name.Size = new System.Drawing.Size(81, 21);
+            this.cb_param_name.Size = new System.Drawing.Size(179, 21);
             this.cb_param_name.TabIndex = 5;
             // 
             // btn_add
             // 
-            this.btn_add.Location = new System.Drawing.Point(180, 48);
+            this.btn_add.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_add.Location = new System.Drawing.Point(407, 48);
             this.btn_add.Name = "btn_add";
             this.btn_add.Size = new System.Drawing.Size(75, 23);
             this.btn_add.TabIndex = 6;
